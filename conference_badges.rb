5da7fe677779c8bd5e3ab7998ab10a6d17c25e38
sameer shaik <sameercourse@gmail.com>
2018-11-22 17:41:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+def badge_maker(str)
+  
+  "Hello, my name is #{str}."
+  
+end 
+
+
+
+def batch_badge_createor(arr)
+  
+  arr1=[];
+  
+  arr.each do |str|
+    
+    arr1.push(badge_maker(str))
+    
+  end 
+  
+  return arr1
+  
+end 
+
+de
+arr1=[]
+roos(arr)
+  
+  arr.each do |str| 
+  
+    
+  end
+  
+end 
+
+
+
